bbea15410baf55124c0326756b6fd16717173a8e
Hariomptdr <patidarh178@gmail.com>
2026-07-02 09:44:39 +0530

    Pointer .

--- Pointers/passByReferenceUsingAlias.cpp
+++ Pointers/passByReferenceUsingAlias.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
-void swap(int *p1, int *p2){
-    int temp = *p1;
-    *p1 = *p2;
-    *p2 = temp;
+void swap(int &x, int &y){
+    int temp = x;
+    x = y;
+    y = temp;
     return;
 }
 int main(){
     int x = 10;
     int y = 20;
-    swap(&x, &y);
+    swap(x, y);
     cout << x <<" "<< y;
 
 
---
+++ Pointers/pointerArithmatic.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int x = 10;
+    // int *ptr = &x;
+    // cout << ptr <<endl;//0x16d9f28bc
+    // ptr = ptr + 1;
+    // cout << ptr << endl;//0x16d9f28c0
+
+    // bool flag = true;
+    // bool *ptr = &flag;
+    // cout << ptr <<endl;//0x16d0be8bf
+    // ptr = ptr + 1;
+    // cout << ptr <<endl;//0x16d0be8c0
+
+    // int x = 10;
+    // int *ptr = &x;
+    // cout << *ptr << endl;// 4
+    // ptr = ptr + 1;
+    // cout << *ptr << endl;// random value
+
+
+}
